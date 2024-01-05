d557594ea3f21b800463f31bd9f8afbb3922f19c
Kevin Gilpin <kgilpin@gmail.com>
2024-01-05 14:05:14 -0500

    feat: Add form-based login

--- src/main/java/org/springframework/samples/petclinic/security/WebSecurityConfig.java
+++ src/main/java/org/springframework/samples/petclinic/security/WebSecurityConfig.java
@@ -17,7 +17,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    http.authorizeRequests().anyRequest().authenticated();
+    http
+        .formLogin()
+        .loginPage("/login")
+        .permitAll();
   }
 
   @Override
